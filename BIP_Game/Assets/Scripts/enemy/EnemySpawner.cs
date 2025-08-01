af330bb02494f7b51fda9777040f8dbed9cb913a
Tiago06Oliveira <tiagompoliveira.06@gmail.com>
2025-08-01 10:59:10 +0300

    spaning enemies

---
+++ BIP_Game/Assets/Scripts/enemy/EnemySpawner.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private float _minimumSpawnTime;
+    [SerializeField]
+    private float _maximumSpawnTime;
+
+    private float _timeUntilSpawn;
+
+    void Awake()
+    {
+        SetTimeUntilSpawn();
+    }
+
+    void Update()
+    {
+        _timeUntilSpawn -= Time.deltaTime;
+
+        if (_timeUntilSpawn <= 0f)
+        {
+            Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+    }
+
+    private void SetTimeUntilSpawn()
+    {
+        _timeUntilSpawn = Random.Range(_minimumSpawnTime, _maximumSpawnTime);
+    }
+}
