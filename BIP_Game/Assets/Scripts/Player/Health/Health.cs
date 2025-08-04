067e7ea48af69811a551dcb3f92d31881c0d4a89
sultanov0372 <artem.temon1@gmail.com>
2025-08-04 11:20:12 +0200

    update health bar

--- BIP_Game/Assets/Scripts/Player/Health/Health.cs
+++ BIP_Game/Assets/Scripts/Player/Health/Health.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class Health : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
+
+    // Health UI
+    [SerializeField]
+    public Sprite[] heartSprites;
+    [SerializeField]
+    public Image heartImage;
 
     void Start()
     {
@@ -25,10 +34,24 @@ public class Health : MonoBehaviour
         currentHealth = Mathf.Min(currentHealth, maxHealth);
     }
 
+    void Update()
+    {
+        UpdateHealth();
+    }
+
     private void Die()
     {
-        Debug.Log(gameObject.name + " умер");
-        // Здесь можно добавить уничтожение объекта или другую логику
+        Debug.Log(gameObject.name + " dead");
         Destroy(gameObject);
+        SceneManager.LoadSceneAsync("Menu");
+    }
+
+    void UpdateHealth()
+    {
+        float healthPercent = Mathf.Clamp01((float)currentHealth / maxHealth);
+        int spriteIndex = Mathf.RoundToInt(healthPercent * (heartSprites.Length - 1));
+        spriteIndex = Mathf.Clamp(spriteIndex, 0, heartSprites.Length - 1);
+
+        heartImage.sprite = heartSprites[spriteIndex];
     }
 }
