055f478a84d32f29ca4c960cc8d9c29841e03b29
Artem Sultanov <as@intelligentgraphics.ag>
2025-08-04 12:27:53 +0200

    update level

--- BIP_Game/Assets/Scripts/Player/EXP/Experience.cs
+++ BIP_Game/Assets/Scripts/Player/EXP/Experience.cs
@@ -1,5 +1,7 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 
 public class Expirience : MonoBehaviour
@@ -11,15 +13,25 @@ public class Expirience : MonoBehaviour
     public Image expImage;
 
     public int maxExp = 8;
-
-
     static public int EXP = 0;
+    private int level = 1;
 
-    
+    public TMP_Text levelText;      // ← Assign a UI Text element
+
+
+    private void Start()
+    {
+        levelText.text = "1";
+    }
+
     void Update()
     {
         print(EXP);
         UpdateExp();
+        if (EXP >= maxExp)
+        {
+            LevelUp();
+        }
     }
     void UpdateExp()
     {
@@ -28,5 +40,13 @@ public class Expirience : MonoBehaviour
         spriteIndex = Mathf.Clamp(spriteIndex, 0, expSprites.Length - 1);
 
         expImage.sprite = expSprites[spriteIndex];
+    }
+    void LevelUp()
+    {
+        EXP -= maxExp;  // Carry over leftover EXP
+        level++;
+        levelText.text = level.ToString();
+
+        Debug.Log("Level Up! New level: " + level);
     }
 }
