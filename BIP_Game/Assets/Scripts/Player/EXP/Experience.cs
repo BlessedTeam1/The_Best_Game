8512a84d1f8bf8878e35655f564670f74b522f01
Artem Sultanov <as@intelligentgraphics.ag>
2025-08-04 12:01:50 +0200

    update exp

--- BIP_Game/Assets/Scripts/Player/EXP/Experience.cs
+++ BIP_Game/Assets/Scripts/Player/EXP/Experience.cs
@@ -1,11 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Expirience : MonoBehaviour
 {
+    // Health UI
+    [SerializeField]
+    public Sprite[] expSprites;
+    [SerializeField]
+    public Image expImage;
+
+    public int maxExp = 8;
+
 
     static public int EXP = 0;
+
+    
     void Update()
     {
         print(EXP);
+        UpdateExp();
+    }
+    void UpdateExp()
+    {
+        float expPercent = Mathf.Clamp01((float)EXP / maxExp);
+        int spriteIndex = Mathf.RoundToInt(expPercent * (expSprites.Length - 1));
+        spriteIndex = Mathf.Clamp(spriteIndex, 0, expSprites.Length - 1);
+
+        expImage.sprite = expSprites[spriteIndex];
     }
 }
